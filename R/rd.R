f8a4102f3ee6f4e142b3d9fc1a1b7c2f689a8281
renaud <renaud@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-25 07:19:47 +0000

    Add internal functions to extract/convert Rd sections to Latex
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/pkgmaker/pkg/pkgmaker@166 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/rd.R
@@ -0,0 +1,34 @@
+# Rd utility functions
+# 
+# Author: Renaud Gaujoux
+# Created: Mar 25, 2013
+###############################################################################
+
+getRdFile <- function(topic, package=NULL){
+	help_call <- substitute(help(topic, package = package, try.all.packages = TRUE), 
+			list(topic = topic, package = package))
+	
+	eval(help_call)
+}
+
+getRdTag <- function(topic, tag, package=NULL){
+	# extract topic
+	rd <- utils:::.getHelpFile(file=getRdFile(topic, package=package))
+	w <- which(tools:::RdTags(rd) == tag)
+	rd[w]
+}
+
+RdSection2latex <- function(topic, i=1L, notitle=TRUE, ...){
+	rdsec <- getRdTag(topic, tag="\\section", ...)
+	if( !length(rdsec) ) return()
+	ltx <- capture.output(tools::Rd2latex(rdsec[i], fragment=TRUE))
+	if( notitle ){
+		parts <- stringr::str_match(ltx, "\\{Section\\}")
+		w <- which(!is.na(parts[, 1]))
+		ltx <- ltx[seq(w[1]+1, tail(w, 1)-1)]
+	}
+	ltx <- paste(ltx, collapse="\n")
+	cat(ltx)
+	invisible()
+}
+
