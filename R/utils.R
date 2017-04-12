741a425c97b53f3a13d7c08e23c73b5d9487f73a
Renaud <renaud@tx.technion.ac.il>
2017-04-12 11:09:59 +0200

    Added function gfile

--- R/utils.R
+++ R/utils.R
@@ -1122,3 +1122,44 @@ charmap <- function(x, maps, nomatch = NULL, partial = FALSE, rev = FALSE){
 	as.character(i)
 }
 
+
+#' Open a File Graphic Device
+#'
+#' Opens a graphic device depending on the file extension.
+#' 
+#' @param filename path to the image file to create.
+#' @param width output width
+#' @param height output height
+#' @param ... other arguments passed to the relevant device function
+#' such as \code{\link{png}} or \code{\link{pdf}}.
+#' 
+#' importFrom grDevices bmp jpeg pdf png svg tiff
+#' @export
+gfile <- function(filename, width, height, ...){ 
+  # Get file type
+  r = regexpr("\\.[a-zA-Z]*$", filename)
+  if(r == -1) stop("Improper filename")
+  ending = substr(filename, r + 1, r + attr(r, "match.length"))
+  
+  f = switch(ending,
+      pdf = function(x, ...) pdf(x, ...),
+      svg = function(x, ...) svg(x, ...),			
+      png = function(x, ...) png(x, ...),
+      jpeg = function(x, ...) jpeg(x, ...),
+      jpg = function(x, ...) jpeg(x, ...),
+      tiff = function(x, ...) tiff(x, compression = "lzw", ...),
+      bmp = function(x, ...) bmp(x, ...),
+      stop("File type should be: pdf, svg, png, bmp, jpg, tiff")
+  )
+  
+  args <- c(list(filename), list(...))	
+  if( !missing(width) ){
+    args$width <- as.numeric(width)
+    args$height <- as.numeric(height)
+    if( !ending %in% c('pdf','svg') && is.null(args[['res']]) ){
+      args$units <- "in"
+      args$res <- 300
+    }
+  }
+  do.call('f', args)	
+}
