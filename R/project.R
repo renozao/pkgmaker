200ef3a9d78380a9d7b6ddc93880af73212651b5
Renaud <renaud@tx.technion.ac.il>
2017-04-18 16:17:01 +0200

    Enhanced project loading by allowing regular expression in .Rpackages

--- R/project.R
+++ R/project.R
@@ -4,6 +4,25 @@
 # Created: May 1, 2013
 ###############################################################################
 
+is_pattern <- function(x){
+  grepl('[*^?)$([]', x)
+}
+# match with exact or regular expression lookup
+match_mix <- function(x, table, nomatch = NA_integer_){
+  
+  # find regular expression patterns
+  is_reg <- is_pattern(table)
+  hit <- match(x, table[!is_reg], nomatch = nomatch)
+  
+  if( any(is_reg) ){
+    reg_match <- unlist_(sapply(table[is_reg], grep, x, simplify = FALSE))
+    reg_match <- reg_match[!duplicated(reg_match)]
+    hit[reg_match] <- pmin(hit[reg_match], match(names(reg_match), table), na.rm = TRUE)
+    
+  }
+  
+  hit[!is.na(hit)]
+}
 
 #' Test for Package Root Directory
 #' 
@@ -35,6 +54,14 @@ is_package_path <- function(x, error = FALSE) {
 #' Development packages are looked-up according to rules
 #' defined in a file \code{.Rpackages} in the user's home directory. 
 #' 
+#' @section Specification of package path:
+#' Package paths are specified in a list with:
+#'   * unnamed elements: character strings give path to directories to lookup for sub-directories that match
+#' exactly the package's name;
+#'   * named element containing character strings: these are paths that are looked up only for packages that 
+#' match the element name. If the element name contains any of the characters `*?()$^\\][`, then it is matched using 
+#' regular expression.
+#' 
 #' @param x name of the development package to lookup.
 #' @export
 find_devpackage <- function (x) 
@@ -51,12 +78,50 @@ find_devpackage <- function (x)
 	}
 	config_path <- path.expand(config_path)
 	lookup <- source(config_path)$value
-	if (is_package_path(lookup[[x]])) {
-		return(lookup[[x]])
-	}
-	if (!is.null(lookup$default)) {
-		default_loc <- lookup$default(x)
+  
+  default_lookup <- lookup$default
+  lookup <- lookup[!names(lookup) %in% 'default']
+  
+  # check for a match
+  i <- c(match_mix(x, names(lookup)), which(names(lookup) %in% ''))
+  if( length(i) ){
+      reg_spec <- is_pattern(names(lookup))
+      for(k in i){
+        val <- lookup[[k]]
+        n <- names(lookup)[k]
+        if( grepl('*', val, fixed = TRUE) ){ # check path like a/b/*/c
+          p <- gsub('*', x, val, fixed = TRUE)
+          if( is_package_path(p) ){
+            message("Loading path resolved by ", n, ':', val)
+            return(p)
+            
+          }
+          
+        } else if( !reg_spec[k] && is_package_path(val) ){ # exact match
+          message("Loading path resolved by ", n, ':', val)
+          return(val) 
+          
+        }else if( reg_spec[k] ){
+          paths <- list.dirs(val, full.names = TRUE, recursive = FALSE)
+          x2 <- sub(n, "", x)
+          x2[!nzchar(x2)] <- x[!nzchar(x2)]
+          hit <- match(x2, basename(paths))
+          p <- paths[hit[1L]]
+          if( length(hit) && is_package_path(p) ){
+            message("Loading path resolved by ", n, ':', val)
+            return(p)
+            
+          }
+          
+        }
+      }
+      
+  }
+  
+  if( !is.null(default_lookup) ) {
+		default_loc <- default_lookup(x)
 		if (is_package_path(default_loc, error = TRUE)) {
+      message("Loading path resolved by default lookup")
 			return(default_loc)
 		}
 	}
