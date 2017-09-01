1641ad0192d5ee30f0ee17de0822a068b7a5e73a
Renaud <renaud@tx.technion.ac.il>
2017-09-01 09:21:16 +0300

    Converted unit tests from RUnit into testthat

--- tests/doRUnit.R
+++
@@ -1,6 +0,0 @@
-# Run all unit tests in installed directory unitTests
-# 
-# Author: Renaud Gaujoux
-###############################################################################
-
-pkgmaker::utest('package:pkgmaker', quiet=FALSE)
